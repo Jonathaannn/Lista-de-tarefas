abea6aa3f2c705800a8331c67191253bb97014c0
Jonathan César Reis dos Santos <jonathan237cjz2@gmail.com>
2022-02-10 12:57:00 -0300

    Começando a manipulação do DOM em javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const adcItem = document.querySelector('#adciona-item');
+const adcBotao = document.querySelector('#adciona-botao');
+const lstFazer = document.querySelector('#lista-fazer');
+
+adcBotao.addEventListener('click', adclista)
+
+function adclista(event){
+    event.preventDefault()
+}
